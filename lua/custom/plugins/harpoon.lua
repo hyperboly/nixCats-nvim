2a52f897733cdd7e8dac2949100de57df5839c01
hyperboly <johnwuonmail@gmail.com>
2026-01-15 21:19:20 +0800

    remove old comments

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,6 +1,6 @@
 return {
   'ThePrimeagen/harpoon',
-  branch = 'harpoon2', -- optional, but recommended these days
+  branch = 'harpoon2',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope.nvim',
@@ -18,14 +18,3 @@ return {
     vim.keymap.set('n', '<C-p>', ui.nav_prev, { desc = 'Previous Harpoon file' })
   end,
 }
---
---
--- local mark = require 'harpoon.mark'
--- local ui = require 'harpoon.ui'
--- require('telescope').load_extension 'harpoon'
---
--- vim.keymap.set('n', '<leader>a', mark.add_file, { desc = '[A]dd file to marks' })
--- vim.keymap.set('n', '<C-e>', ':Telescope harpoon marks<CR>', opts)
--- vim.keymap.set('n', '<C-m>', ui.toggle_quick_menu)
--- vim.keymap.set('n', '<C-n>', ui.nav_next, { desc = '[N]ext harpooned file' })
--- vim.keymap.set('n', '<C-p>', ui.nav_prev, { desc = '[P]revious harpooned file' })
