8473abdb1ae8aa492eb6451b755c655244c5819a
hyperboly <johnwuonmail@gmail.com>
2026-01-15 21:13:22 +0800

    harpoon

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,31 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2', -- optional, but recommended these days
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+  },
+  config = function()
+    local mark = require 'harpoon.mark'
+    local ui = require 'harpoon.ui'
+
+    require('telescope').load_extension 'harpoon'
+
+    vim.keymap.set('n', '<leader>a', mark.add_file, { desc = '[A]dd file to Harpoon' })
+    vim.keymap.set('n', '<C-e>', '<cmd>Telescope harpoon marks<CR>', { desc = 'Harpoon marks' })
+    vim.keymap.set('n', '<C-m>', ui.toggle_quick_menu, { desc = 'Harpoon menu' })
+    vim.keymap.set('n', '<C-n>', ui.nav_next, { desc = 'Next Harpoon file' })
+    vim.keymap.set('n', '<C-p>', ui.nav_prev, { desc = 'Previous Harpoon file' })
+  end,
+}
+--
+--
+-- local mark = require 'harpoon.mark'
+-- local ui = require 'harpoon.ui'
+-- require('telescope').load_extension 'harpoon'
+--
+-- vim.keymap.set('n', '<leader>a', mark.add_file, { desc = '[A]dd file to marks' })
+-- vim.keymap.set('n', '<C-e>', ':Telescope harpoon marks<CR>', opts)
+-- vim.keymap.set('n', '<C-m>', ui.toggle_quick_menu)
+-- vim.keymap.set('n', '<C-n>', ui.nav_next, { desc = '[N]ext harpooned file' })
+-- vim.keymap.set('n', '<C-p>', ui.nav_prev, { desc = '[P]revious harpooned file' })
