58bde5db9a2947200636254d670ec9a270b0cdea
hyperboly <johnwuonmail@gmail.com>
2026-04-04 18:04:32 +0800

    add: harpoon descriptions and updated inputs

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -12,11 +12,11 @@ return {
     harpoon:setup()
     -- REQUIRED
 
-    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = 'add file to harpoon list' })
+    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'toggle harpoon menu' })
 
     -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
-    vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
+    vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end, { desc = 'toggle previous harpoon buffer' })
+    vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end, { desc = 'toggle next harpoon buffer' })
   end,
 }
